de3892d02442ebea721135504cc49582ca4d8fc2
GNANASHREE VIJAYARAGUNATHAN <gnanashreevijayaragunathan@gmail.com>
2026-02-04 17:49:16 +0530

    Add files via upload

---
+++ src/Course.jsx
@@ -0,0 +1,10 @@
+function Course( ) {
+    return(
+        <div>
+            <img src="" alt=""/>
+            <h3>HTML</h3>
+            <p>This is code io HTML course</p>
+        </div>
+    );
+}
+export default Course
